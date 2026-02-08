7089a5d1aa96cf2fecbeed0352019c64cc8efbbd
Piyush <piyush72717272@gmail.com>
2026-02-08 21:52:08 +0530

    feat: Implement Google Sign-In button
    This commit introduces the functionality for the "Sign in with Google" button on the `LoginScreen`.
    
    - Converts `LoginScreen` from a `StatelessWidget` to a `ConsumerWidget`.
    - Implements `onGoogleSignInTap` to handle the Google Sign-In process using the `AuthRepository`.
    - Upon successful sign-in, the user's access token, display name, email, and photo URL are printed.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,24 +1,37 @@
 import 'package:brain_note/colors.dart';
+import 'package:brain_note/repostiory/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
+  void onGoogleSignInTap(WidgetRef ref)async {
+    try{
+      final authProvider = ref.read(authRepositoryProvider);
+      final googleSignInAccount = await authProvider.signIn();
+      if(googleSignInAccount!=null){
+        final accessToken = await authProvider.getAccessToken(googleSignInAccount);
+        print(accessToken);
+        print(googleSignInAccount.displayName);
+        print(googleSignInAccount.email);
+        print(googleSignInAccount.photoUrl);
+      }
+    }catch(e){
+      print(e);
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () {},
-          icon: Image.asset(
-            'assets/images/g-logo-2.png',
-            height: 20,
-          ),
+          onPressed: () => onGoogleSignInTap(ref),
+          icon: Image.asset('assets/images/g-logo-2.png', height: 20),
           label: const Text(
             'Sign in with Google',
-            style: TextStyle(
-              color: kBlackColor,
-            ),
+            style: TextStyle(color: kBlackColor),
           ),
           style: ElevatedButton.styleFrom(
             backgroundColor: kWhiteColor,
