9731bda3386eb447e0f8afcc265567b9ec1a0eab
Piyush <piyush72717272@gmail.com>
2026-02-13 22:00:20 +0530

    feat: Add Cross-Origin headers for enhanced security

--- server/index.js
+++ server/index.js
@@ -26,6 +26,17 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    res.setHeader(
+        'Cross-Origin-Opener-Policy',
+        'same-origin-allow-popups'
+    );
+    res.setHeader(
+        'Cross-Origin-Embedder-Policy',
+        'unsafe-none'
+    );
+    next();
+});
 
 /* =====================================================
    ROUTES
